148c6b1636e8365388d6fcc674fbf53ae53ee9d7
Marcus <mcavini@outlook.com>
2019-05-14 10:23:27 -0400

    Create users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+    def new
+        render :new
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        render :show
+    end 
+
+    def create  
+        @user = User.new(user_params)
+        if @user.save
+            login(@user)
+            redirect_to vehicles_url
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end 
+    end 
+
+    def user_params
+        params.require(:user).permit(:email, :password, :first_name, :last_name)
+    end
+
+end 
